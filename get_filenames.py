c2fa696aa76fd75b4769f8ffa6a1427654fbac12
WeiyunJiang <wyjiang@stanford.edu>
2021-11-09 21:50:58 -0800

    text file for filenames

---
+++ get_filenames.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 21:37:21 2021
+
+@author: weiyunjiang
+"""
+import os
+import pickle 
+
+train_data_dir = "./anime_data/train"
+train_filenames = os.listdir(train_data_dir) 
+with open('train_filenames', 'wb') as f:
+    pickle.dump(train_filenames, f)
+
+
+val_data_dir = "./anime_data/val"
+val_filenames = os.listdir(val_data_dir) 
+with open('val_filenames', 'wb') as f:
+    pickle.dump(val_filenames, f)
+    
+    
+
+# with open('train_filenames', 'rb') as f:
+#     mynewlist = pickle.load(f)
+    
+pass
